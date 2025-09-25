f2cce21dba10567142d91fc21bbf5ce0f11c239c
faverim <gfaverim@gmail.com>
2025-09-25 11:20:35 -0300

    Add files via upload

---
+++ salario mensal.py
@@ -0,0 +1,8 @@
+valor_hora = float(input("digite o quanto voce ganha por hora: "))
+horas_mês = float(input("digite o numero de horas trabalhadas no mês: "))
+
+salario = (valor_hora * horas_mês)
+
+print("seu salario é: R$ {salario}\n")
+
+
